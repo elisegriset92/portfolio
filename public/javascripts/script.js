bf3a927715e098af649928e1ef79b5151b19a5c0
Elise Griset <elise.griset@gmail.com>
2018-05-21 15:59:41 +0200

    yolo

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener (
+  'DOMContentLoaded',
+  () => {
+    console.log ('IronGenerator JS imported successfully!');
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+$ (document).ready (function () {
+  console.log ('ready!');
+});
 
-}, false);
+// initialize paroller.js
+$ ('[data-paroller-factor]').paroller ({
+  factor: '0.5',
+  type: 'foreground',
+  direction: 'horizontal',
+});
